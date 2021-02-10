f03c47b889fef5e45206dae886dfccdbb6ecaa16
Karin <ka_deutsch@hotmail.ch>
2021-02-10 20:32:25 +0100

    header + assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      {/*items => snapping */}
+      {/*items => snapping */}
+      {/*items => snapping */}
+      {/*items => snapping */}
+      {/*items => snapping */}
     </div>
   );
 }
